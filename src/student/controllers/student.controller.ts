8e6afe9f7d7b9fa00135cb2b8db012416d3e583f
Sonali-Mali93 <sonalimali999@gmail.com>
2022-08-15 23:27:50 +0530

    feat: student service is created

---
+++ src/student/controllers/student.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+import { StudentService } from '../services/student.service';
+
+@Controller('student')
+export class StudentController {
+  constructor(private studentService: StudentService) {}
+
+  
+}
